84b5d00845476a134ff43d6c438d7a98a73ced15
hogget03 <stepanenkoa167@gmail.com>
2023-05-24 20:33:37 +0300

    do ex 3, have qshns about 2 & 4

--- src/main/java/by/academy/HW/Ex3.java
+++ src/main/java/by/academy/HW/Ex3.java
@@ -1,5 +1,18 @@
 package by.academy.HW;
 
+import java.util.Scanner;
+
 public class Ex3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите цифру от 1 до 10");
+		int n = sc.nextInt();
+		sc.close();
+		for (int k = 1; k < 11; k++) {
+			System.out.print(n + "*" + k + "=" + k * n + "   ");
+		}
+
+	}
 
 }
