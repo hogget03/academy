ec328bc7d527fcadf3e50cd6fa16d11fb4d965a4
hogget03 <stepanenkoa167@gmail.com>
2023-06-04 13:47:42 +0300

    do all Cat

---
+++ src/main/java/HW2/EX1.java
@@ -0,0 +1,10 @@
+package HW2;
+
+public class EX1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/by/academy/lesson1/Cat.java
+++ src/main/java/by/academy/lesson1/Cat.java
@@ -1,42 +1,78 @@
 package by.academy.lesson1;
 
 public class Cat {
-
-	String nikname;
+	int age;
+	String nickname;
 	String color;
 	double weight;
-	int age;
-	double height;
 	int money;
+	char initials;
+	boolean isHomeAnimal;
 
 	public Cat() {
 		super();
+
 	}
 
-	public Cat(String nikname, String color, double weight, int age) {
+	public Cat(String nickname, String color, int age, double weight) {
 		super();
-		this.nikname = nikname;
+		this.age = age;
+		this.nickname = nickname;
 		this.color = color;
 		this.weight = weight;
-		this.age = age;
 	}
 
-	public void sleep() {
-		System.out.println(nikname + " спит");
+	public void grow() {
+
+		age = age + 1;
+
+	}
+
+	public void slep() {
+		System.out.println("Кот " + nickname + " спит");
 	}
 
 	public void eat() {
 		weight = weight + 1;
-		System.out.println(nikname + " ест , вес кота теперь " + weight);
+		System.out.println("Кот " + nickname + " ест , его вес теперь " + weight);
+
 	}
 
 	public void walk() {
 		weight = weight - 1;
-		System.out.println(nikname + " гуляет , теперь вес кота " + weight);
+		System.out.println("Кот " + nickname + " гуляет , теперь его вес  " + weight);
+
+	}
+
+	public int getMoney() {
+		return money;
+
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public char getInitials() {
+		return initials;
+	}
+
+	public void setInitials(char initials) {
+		this.initials = initials;
+	}
+
+	public boolean isHomeAnimal() {
+		return isHomeAnimal;
+	}
+
+	public void setHomeAnimal(boolean isHomeAnimal) {
+		this.isHomeAnimal = isHomeAnimal;
 	}
 
 	@Override
 	public String toString() {
-		return "nikname :" + nikname + "color :" + color + "weight :" + weight + "age :" + age;
+		return "nickname: " + nickname + ", color: " + color + ", weight: " + weight + ", age: " + age;
+
 	}
+
 }
